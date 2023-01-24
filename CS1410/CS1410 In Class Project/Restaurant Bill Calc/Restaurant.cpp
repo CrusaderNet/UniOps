711e63222576d341c048beb7e037dea8e135808e
CrusaderNet <stourish@comcast.net>
2023-01-24 10:50:48 -0600

    New In Class Lesson Code, Restaurant Meal Bill Calculator

---
+++ CS1410/CS1410 In Class Project/Restaurant Bill Calc/Restaurant.cpp
@@ -0,0 +1,32 @@
+//Seth T.
+// January 24, 2023
+//In Class Activity, Restaurant Bill Calculator
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	
+	const double TAX_RATE = 0.0675;
+	const double TIP_RATE = 0.020;
+
+	double subtotal;
+
+	cout << "Enter the Meal Cost: ";
+	cin >> subtotal;
+
+	double tax = subtotal * TAX_RATE;
+	double tip = (subtotal + tax) * TIP_RATE;
+
+	double total = subtotal + tax + tip;
+
+	cout << fixed << showpoint << setprecision(2);
+	cout << "\n-------- BILL --------\n" << "    Meal Cost: $" << subtotal << endl << "    Tax Amount: $" << tax << endl << "    Tip Amount: $" << tip << endl << "    Total Bill: $" << total << endl;
+
+	return 0;
+
+}
