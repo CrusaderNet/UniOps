a94c663530e5173b73ac4295f0daf5f2b59da82b
CrusaderNet <110443549+CrusaderNet@users.noreply.github.com>
2023-12-03 22:47:19 -0600

    Edited HW10_Team8.cpp CS2410

--- CS2410/CS2410/CS2410/Homework/HW10_Team8/HW10_Team8.cpp
+++ CS2410/CS2410/CS2410/Homework/HW10_Team8/HW10_Team8.cpp
@@ -1,26 +1,35 @@
-//Team 8 - (Milly Flores 50%,Seth Tourish 50%)
-// This program demonstrates a simple append
-// operation on a linked list.
+//Team 8 - (Milly Flores 50%, Seth Tourish 50%)
+
+//Preprocessor directives
 #include <iostream>
 #include "NumberList.h"
+
+//Namespace declaration
 using namespace std;
+
+//Main function
 int main()
 {
-	// Define a NumberList object.
+	// Define a NumberList object
 	NumberList list;
 	double val;
-	// Append some values to the list.
+	// Append some values to the list
 	list.appendNode(2.5);
 	list.appendNode(7.9);
 	list.appendNode(12.6);
+	// Display the list
 	list.displayList();
+	// Display the total of the values in the list
 	cout << "The total of the values in the list is " << list.total() << endl << endl;
 
+	//Search for a value, take in user input
 	cout << "Enter a value to search for: ";
 	cin >> val;
 	cout << endl;
 
-	 val = list.search(val);
+	//Call search function
+	val = list.search(val);
+	//Display results
 	if (val != -1)
 	{
 		cout << "The value you are searching for is in position " << val << endl;
