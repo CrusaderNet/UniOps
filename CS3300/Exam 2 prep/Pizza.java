bdb580829e0633811dc1e87388225e5f46518f21
CrusaderNet <110443549+CrusaderNet@users.noreply.github.com>
2024-04-03 09:13:26 -0500

    Rewrote Pizza class as one java file

--- CS3300/Exam 2 prep/Pizza.java
+++ CS3300/Exam 2 prep/Pizza.java
@@ -1,13 +1,14 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.pizzaclass;
+
+package com.mycompany.pizza;
 
 /**
  *
  * @author Seth
  */
+import java.util.Scanner;
 
 public class Pizza{
     
@@ -55,5 +56,44 @@ public class Pizza{
             
             return desc;
         }
+       
+        public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int number, cT, pT, hT;
+        double total = 0;
+        String input, size;
+        
+        System.out.print("How many Pizzas do you want?: ");
+        input = sc.next();
+        number = Integer.parseInt(input);
+        
+        for(int i = 0; i < number; i++){
+            System.out.print("What size pizza do you want your pizza to be: Small, Medium or Large?: ");
+            size = sc.next();
+            
+            System.out.print("How many cheese toppings do you want?: ");
+            input = sc.next();
+            cT = Integer.parseInt(input);
+            
+            System.out.print("How many pepperoni toppings do you want?: ");
+            input = sc.next();
+            pT = Integer.parseInt(input);
+            
+            System.out.print("How many ham toppings do you want?: ");
+            input = sc.next();
+            hT = Integer.parseInt(input);
+            
+            Pizza pie = new Pizza(size, cT, pT, hT);
+            
+            System.out.println(pie.getDescription());
+            total += pie.calcCost();
+            
+            
+        }
+        
+        System.out.println("The total cost is: " + total);
         
     }
+}
